420e106ecd351fc1237db72796a9f6965eb332c9
John Kelleher <johnpkelleher@gmail.com>
2016-03-29 18:09:02 -0400

    rescue in favicon dot ruby and adapting favicon dot ruby to new schema

--- app/models/favicon.rb
+++ app/models/favicon.rb
@@ -6,26 +6,38 @@ class Favicon < ActiveRecord::Base
   def self.import(file)
     count = 0
     CSV.foreach(file.path, headers: false) do |row|
-      self.find_favicon "http://#{row[1]}"
-      count += 1
+      begin
+        self.find_favicon "http://#{row[1]}"
+        count += 1
+      rescue
+        logger.info "could not find #{row[1]}"
+      end
       break if count > 10
     end
   end
 
-  def self.find_favicon(home_page_url)
-    favicon = Favicon.find_by(site_url: home_page_url)
+  #TODO add get fresh option to do another lookup even if one exists in db
+  def self.find_favicon(requested_url) #note requested_url is the result of user input and treatment by home_controller
+    favicon = Favicon.find_by(site_url: requested_url)
     if favicon.present?
-      favicon_url = favicon.favicon_url
+      favicon
     else
-      self.fetch_favicon(home_page_url)
+      self.fetch_favicon(requested_url)
     end
   end
 
-  def self.fetch_favicon(home_page_url)
-    page_results = open(home_page_url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, :allow_redirections => :safe})
+  def self.fetch_favicon(requested_url)
+    page_results = open(requested_url, {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE, :allow_redirections => :safe})
     base_uri = page_results.base_uri
-    site_url = "#{base_uri.scheme}://#{base_uri.host + base_uri.path}"
+    site_url_path = base_uri.path.length > 1 ? base_uri.path : ''
+    site_url_scheme = base_uri.scheme
+    site_url_host = base_uri.host
+
+    site_url = "#{base_uri.scheme}://#{base_uri.host + site_url_path}"
+    
+    #TODO check for size of favicon, if 0 look for link tags on page and grab one
     favicon_url = "#{base_uri.scheme}://#{base_uri.host}/favicon.ico"
-    Favicon.create(site_url: site_url, favicon_url: favicon_url)
+
+    Favicon.create(site_url_scheme: site_url_scheme, site_url_host: site_url_host, site_url_path: site_url_path, requested_url: requested_url, favicon_url: favicon_url)
   end
 end
