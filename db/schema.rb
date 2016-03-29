7587ba423e00e3d89c23dc8127605903fa413b65
John Kelleher <johnpkelleher@gmail.com>
2016-03-29 18:01:32 -0400

    ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328235645) do
+ActiveRecord::Schema.define(version: 20160329215812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +19,12 @@ ActiveRecord::Schema.define(version: 20160328235645) do
   create_table "favicons", force: :cascade do |t|
     t.string   "site_url"
     t.string   "favicon_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "site_url_scheme"
+    t.string   "site_url_host"
+    t.string   "site_url_path"
+    t.string   "requested_url"
   end
 
 end
